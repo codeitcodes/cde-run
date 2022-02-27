f379f0d76b1da7f7c6d111bee7cd17f7f94a0431
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-02-27 20:48:00 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 
 export default function handler(request, response) {
-  //' + request.query + '
   
-  response.status(200).send('<h1 style="font-family:system-ui">Hello world!</h1>');
+  const html = '<h1 style="font-family:system-ui">' + request.query + '</h1>';
+  
+  response.status(200).send(html);
+  
 }
